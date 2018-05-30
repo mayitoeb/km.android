e7837b2e53ec212d26912046b8f870dcf1d99f07
Mario E Bello <mayito.eb@gmail.com>
2018-05-29 21:21:44 -0400

    loads ad on home page, adservice added and ad model added

---
+++ src/models/ad.ts
@@ -0,0 +1,6 @@
+export class Ad {
+    id: string;
+    title: string;
+    subtitle: string;
+    photoUrl: string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Ad } from '../../models/ad';
+import { AdService } from '../../services/ad.service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  ads: Ad[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private adService: AdService) {}
 
-  }
+    ngOnInit(): void {
+      this.adService.getAds()
+      .subscribe(ads => {
+        this.ads = ads;
+      })
+    }
 
 }
